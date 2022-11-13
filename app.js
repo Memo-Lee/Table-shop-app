2e3e03555daa64d89c6bd226c19201615b38da60
MemoLee <softwarewewo@gmail.com>
2022-11-13 23:59:58 +0300

    Template Engineadded

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const express = require('express')
+const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
-const app = express()
+const app = express();
+
+// TEMPLATE ENGINE
+app.set("view engine","ejs");
+
 //MIDDLEWARES
+/* request - response döngüsünün içerisindeki görevi olan her fonksiyona 
+middleware denir. Yani herşey request ve responsun 'middle'ında ortasında yapılır. */
 app.use(express.static('public'))
 
+// ROUTES
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/add', (req, res) => {
+    res.render('add');
 });
 
 const port = 3000;
