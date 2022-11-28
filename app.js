3bd19ed3c78cd6d46e8d7df22b3e8a48de442aa9
MemoLee <softwarewewo@gmail.com>
2022-11-28 14:56:08 +0300

    mvc

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
-const fs = require('fs');
-
-const Photo = require('./models/Photo');
-
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 const app = express();
 
 // connect DB
@@ -32,82 +29,21 @@ app.use(methodOverride('_method',{
 })); // gerektiğinde metotların override olamasını belirtebiliyoruz.
 
 
-
 // ROUTES
-app.get('/', async (req, res) => {
-    // index sayfasın da dinamik verileri görüntüleme işlemi
-    const photos = await Photo.find({}).sort('-dateCreated');
-    // .sort('-dateCreated')
-    res.render('index', {
-        photos
-    });
-});
-app.get('/photos/:id', async (req, res) => {
-    // console.log(req.params.id);
-    // res.render(/photos);
-    // Burada tek bir fotoya ait olan bilgi gönderilir.
-    const findPhoto = await Photo.findById(req.params.id);
-    res.render('photo', {
-        photo:findPhoto
-    });
-});
+app.get('/', photoController.getAllPhotos);
 
-app.post('/photos', async (req, res) => {
-    // console.log(req.body) -> forma girdiğimiz string bilgileri alıyoruz
-    // await Photo.create(req.body);
-    // console.log(req.file.image) -> dosyanın bilg alıyoruz
-    // res.redirect('/'); ->     // posttan sonra yönlendirme
+app.post('/photos', photoController.createPhoto);
 
-    const uploadDir = 'public/uploads';
-    if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir)
-    }
+app.get('/photos/:id', photoController.getPhoto);
 
-    // __dirname projenin kökü , uploadImage dosya , uploadPath dosya yolu
-    let uploadImage = req.files.image;
-    let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+app.put('/photos/:id', photoController.updatePhoto);
 
-    uploadImage.mv(uploadPath, async () => {
-        await Photo.create({
-            ...req.body,
-            image: '/uploads/' + uploadImage.name,
-        });
-    });
-    res.redirect('/');
-});
-app.get('/photos/edit/:id', async (req, res) => {
-    const photo = await Photo.findOne({_id:req.params.id}) //paramsdan gelen underscore id ile eşleşmesi
-    res.render('edit',{
-        photo
-    }); // seçtiğimiz image'i template gönderme
-    // 2. parametre göndermek istediğimiz bilgi
-});
-app.put('/photos/:id', async (req, res) => {
-    const photo = await Photo.findOne({_id:req.params.id}) // update edilecek photo yakalandı
-    photo.title = req.body.title
-    photo.description = req.body.description
-    photo.save();
+app.delete('/photos/:id', photoController.deletePhoto);
 
-    res.redirect(`/photos/${req.params.id}`); // backtick kullanıyor çünkü resmin url uzantısına gitmek istiyoruz.
-});
-app.delete('/photos/:id', async (req,res) =>{
-    // await Photo.findByIdAndRemove(req.params.id); // alternatif 1 - upload image silmiyor.
-    const photo = await Photo.findOne({_id:req.params.id}) // underscoreid ' si = parametreden gelen id
-    let deletedImage = __dirname + '/public' + photo.image; //'/uploads/<imagename>.jpg'
-    fs.unlinkSync(deletedImage);
-    await Photo.findByIdAndRemove(req.params.id);
-    res.redirect('/');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-app.get('/add', (req, res) => {
-    res.render('add');
-});
-app.get('/contact', (req, res) => {
-    res.render('contact');
-});
+app.get('/photos/edit/:id', pageController.getEditPage);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/contact', pageController.getContactPage);
 
 
 const port = 3000;
---
+++ controllers/photoControllers.js
@@ -0,0 +1,63 @@
+const Photo = require('../models/Photo');
+const fs = require('fs');
+
+exports.getAllPhotos = async (req, res) => {
+    // index sayfasın da dinamik verileri görüntüleme işlemi
+    const photos = await Photo.find({}).sort('-dateCreated');
+    // .sort('-dateCreated')
+    res.render('index', {
+        photos
+    });
+}
+
+exports.getPhoto = async (req, res) => {
+    // console.log(req.params.id);
+    // res.render(/photos);
+    // Burada tek bir fotoya ait olan bilgi gönderilir.
+    const findPhoto = await Photo.findById(req.params.id);
+    res.render('photo', {
+        photo: findPhoto
+    });
+}
+
+exports.createPhoto = async (req, res) => {
+    // console.log(req.body) -> forma girdiğimiz string bilgileri alıyoruz
+    // await Photo.create(req.body);
+    // console.log(req.file.image) -> dosyanın bilg alıyoruz
+    // res.redirect('/'); ->     // posttan sonra yönlendirme
+
+    const uploadDir = 'public/uploads';
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir)
+    }
+
+    // __dirname projenin kökü , uploadImage dosya , uploadPath dosya yolu
+    let uploadImage = req.files.image;
+    let uploadPath = __dirname + '/../public/uploads/' + uploadImage.name;
+
+    uploadImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadImage.name,
+        });
+    });
+    res.redirect('/');
+}
+
+exports.updatePhoto = async (req, res) => {
+    const photo = await Photo.findOne({_id:req.params.id}) // update edilecek photo yakalandı
+    photo.title = req.body.title
+    photo.description = req.body.description
+    photo.save();
+
+    res.redirect(`/photos/${req.params.id}`); // backtick kullanıyor çünkü resmin url uzantısına gitmek istiyoruz.
+}
+
+exports.deletePhoto = async (req,res) =>{
+    // await Photo.findByIdAndRemove(req.params.id); // alternatif 1 - upload image silmiyor.
+    const photo = await Photo.findOne({_id:req.params.id}) // underscoreid ' si = parametreden gelen id
+    let deletedImage = __dirname + '/../public' + photo.image; //'/uploads/<imagename>.jpg'
+    fs.unlinkSync(deletedImage);
+    await Photo.findByIdAndRemove(req.params.id);
+    res.redirect('/');
+}
